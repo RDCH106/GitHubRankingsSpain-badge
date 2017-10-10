49577fe7668eb5d1caa4152071ccd0a37170473f
Rubén de Celis Hernández <contact@rdch106.hol.es>
2017-10-10 20:31:40 +0200

    Add ranking loading from json

--- service/ghrankingsAPI.py
+++ service/ghrankingsAPI.py
@@ -1,6 +1,31 @@
 # -*- coding: utf-8 -*-
 
 import linkero.core.linkero as linkero
+import sys
+import traceback
+if sys.version_info > (3, 0):           # Python 3 compatibility
+    import urllib.request as urllib
+else:                                   # Python 2 compatibility
+    import urllib
+import json
+
+base_url = "https://raw.githubusercontent.com/iblancasa/ghrankings/master/"
+
+class Ranking:
+    def __init__(self):
+        self.ranking_list = []
+
+    def add_ranking(self, ranking):
+        self.ranking_list.append(ranking)
+
+def load_ranking():
+    try:
+        response = urllib.urlopen(base_url + 'euskadi/euskadi.json')
+        data = json.loads(response.read().decode())
+        print(data)
+    except:
+        traceback.print_exc()
+        raise
 
 class Position(linkero.Resource):
     def get(self, username):
@@ -10,4 +35,5 @@ class Position(linkero.Resource):
 ## Actually setup the Api resource routing here
 ##
 def load_ghrankingsAPI():
+    load_ranking()
     linkero.api.add_resource(Position, '/position/<username>')
