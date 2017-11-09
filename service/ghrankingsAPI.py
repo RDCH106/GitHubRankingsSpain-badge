94abdea963a122d6c6f4f5c6c4dfc5d481617ccc
Rubén de Celis Hernández <contact@rdch106.hol.es>
2017-11-09 18:04:55 +0100

    Add load config method

--- service/ghrankingsAPI.py
+++ service/ghrankingsAPI.py
@@ -10,22 +10,33 @@ else:                                   # Python 2 compatibility
 import json
 
 base_url = "https://raw.githubusercontent.com/iblancasa/ghrankings/master/"
+ranking = None
 
 class Ranking:
     def __init__(self):
+        self.__config = self.load_config()
         self.ranking_list = []
+
 
     def add_ranking(self, ranking):
         self.ranking_list.append(ranking)
 
-def load_ranking():
-    try:
-        response = urllib.urlopen(base_url + 'euskadi/euskadi.json')
-        data = json.loads(response.read().decode())
-        print(data)
-    except:
-        traceback.print_exc()
-        raise
+    def load_config(self):
+        try:
+            with open('config/api_config.json') as config_file:
+                config = json.load(config_file)
+        except:
+            traceback.print_exc()
+            raise
+
+    def load_ranking(self):
+        try:
+            response = urllib.urlopen(self.__config["base_url"] + 'euskadi/euskadi.json')
+            data = json.loads(response.read().decode())
+            print(data)
+        except:
+            traceback.print_exc()
+            raise
 
 class Position(linkero.Resource):
     def get(self, username):
@@ -35,5 +46,7 @@ class Position(linkero.Resource):
 ## Actually setup the Api resource routing here
 ##
 def load_ghrankingsAPI():
-    load_ranking()
+    #load_ranking()
+    global ranking
+    ranking = Ranking()
     linkero.api.add_resource(Position, '/position/<username>')
